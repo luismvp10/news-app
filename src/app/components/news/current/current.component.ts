8720df14895c7c5404620f6831cecac64f72a80a
Luis <luismvalenp@gmail.com>
2020-01-28 18:43:02 -0600

    international news

--- src/app/components/news/current/current.component.ts
+++ src/app/components/news/current/current.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy } from "@angular/common";
 import { NewsService } from '../../../services/news.service';
 
 @Component({
